57d0886fe4c0c61e93ce9139a114c1f1373195c3
persiyanov <persiyanov@yandex-team.ru>
2017-10-13 17:51:48 +0300

    chapter 7: user signup finished

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
   get 'home', to: 'static_pages#home'
   get 'help', to: 'static_pages#help'
   get 'about', to: 'static_pages#about'
   get 'contacts', to: 'static_pages#contacts'
+
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
